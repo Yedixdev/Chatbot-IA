625822a1d5f0626faff38840999c481878b40706
Sebastian Guerra <149134186+SebastianDevps@users.noreply.github.com>
2025-01-02 17:31:08 -0500

    Connect Api

--- src/api/axiosHelper.ts
+++ src/api/axiosHelper.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://127.0.0.1:5000";
+export const BASE_URL = "https://chatbot-whatsapp-ycnt.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
